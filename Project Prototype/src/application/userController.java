64fc5715b928e6155cca2dc60cd7116fea5ea587
Nick <Nick@DESKTOP-3MO0SMJ>
2021-04-09 15:45:21 -0500

    Added the ability to create a .ct file (string-based data file)
    A .ct or CockTail file is created upon a user entering strings into the
    create scene and pressing the ADD RECIPE button. Current logic for this
    function is still very primitive and lacks the ability for text to wrap
    around in the text-fields.
    
    Currently functions are implemented in the controller Class. Eventually,
    these will have to move to a model, through which, we call functions.
    
    Next, I will prioritize development of the ability to read .ct files in
    the View.fxml scene.

--- Project Prototype/src/application/userController.java
+++ Project Prototype/src/application/userController.java
@@ -1,15 +1,54 @@
 package application;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
-
 import javafx.event.ActionEvent;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 
 public class userController {
+	 	
+	@FXML
+	private TextField instructions;
+
+	@FXML
+	private TextField garnishes;
+
+	@FXML
+	private TextField ingredientList;
+
+	@FXML
+	private TextField cocktailName;
+
+	@FXML
+	private TextField description;
+
+	@FXML
+	private Button addButton;
+	
+
+	public void addRecipe(ActionEvent event) throws IOException {
+		File file = new File(cocktailName.getText().toString() + ".ct");
+		file.createNewFile();
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+			writer.write(cocktailName.getText().toString() + "\n");
+			writer.write(description.getText().toString() + "\n");
+			writer.write(ingredientList.getText().toString() + "\n");
+			writer.write(garnishes.getText().toString() + "\n");
+			writer.write(instructions.getText().toString() + "\n");
+		}
+		
+	}
+	    
+	@FXML    
 	public void changeToMenu(ActionEvent event) throws IOException{
 		Parent menuParent = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
 		Scene menuScene = new Scene(menuParent);
@@ -18,4 +57,5 @@ public class userController {
 		window.setScene(menuScene);
 		window.show();
 	}
+	
 }
